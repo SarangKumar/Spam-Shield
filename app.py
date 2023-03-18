eea43d8e01fad85ca33aad2a85043d284364e7a8
SarangKumar <sarangkumar1578@gmail.com>
2023-03-18 18:37:09 +0530

    made stying changes in main

--- app.py
+++ app.py
@@ -20,29 +20,11 @@ def email():
 def sms():
     return render_template('sms.html')
 
+@app.route("/result")
+def result():
+    spam = 0
 
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html')
-    # return "<p>Hello, World!</p>"
-
-@app.route("/rough")
-def rough():
-    return render_template('rough.html')
-
-@app.route("/email")
-def email():
-    return render_template('email.html')
-
-
-@app.route("/sms")
-def sms():
-    return render_template('sms.html')
+    return render_template('result.html', spam=spam)
 
 
 
